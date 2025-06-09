33814ebf77052b9b4803ac2739af47bba1f97777
LuisPiquinRey <piquin.rey@gmail.com>
2025-06-09 10:58:55 +0200

    Insert character

--- StringExercises.java
+++ StringExercises.java
@@ -7,4 +7,11 @@ public class StringExercises {
     public int searchCharacter(String s, char ch) {
         return s.indexOf(ch);
     }
+    /*
+     * Given a string s, a character c and an integer position pos, the task is to insert the character c into
+     * the string s at the specified position pos.
+     */
+    static String insertChar(StringBuilder sb, char c, int pos) {
+        return sb.insert(pos, c).toString();
+    }
 }
